4906b08c45ee2b2fc865037abe721e01307c393a
Danila <Danila@DESKTOP-44LP0PV>
2025-06-07 03:50:48 +0300

    Мой молчаливый протест Буковый гроб и берёзовый крест Было начало и будет конец Не каждому в ноги падёт Эверест Били по дереву капли дождя Я слышал, как сверху рыдает семья Сыпалась градом на крышку земля Что было можно — отныне нельзя Вместо слёз белёсый гной Стекает по моим щекам Немой вопрос сырой землёй прилип К иссохнувшим губам Мне дали бесконечность, дар Мне показали небеса Но я бы всё сейчас отдал Чтобы ещё раз открыть глаза

--- Split/main.cpp
+++ Split/main.cpp
@@ -1,59 +1,85 @@
 #include<iostream>
 using namespace std;
 
-void Arrays_chet_nechet(int arr[], int even[], int odd[], const int n, int cntChetnoe, int cntNeChetnoe);
+void FillRand(int arr[], const int n);
+void Chetnoe_nechetnoe(int arr[], const int n, int cntChetnoe, int cntNeChetnoe);
+void Even(int arr[], int even[], const int n);
+void Odd(int arr[], int odd[], const int n);
 void Print(int arr[], const int n);
 
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	
+
 	const int n = 10;
 	int arr[n];
 	int cntChetnoe = 0;
 	int cntNeChetnoe = 0;
-	int* even = new int[cntChetnoe];
-	int* odd = new int[cntNeChetnoe];
+	int* even = new int[n];
+	int* odd = new int[n];
 
-	Arrays_chet_nechet(arr, even, odd, n, cntChetnoe, cntNeChetnoe);
+	FillRand(arr, n);
+	Chetnoe_nechetnoe(arr, n, cntChetnoe, cntNeChetnoe);
+	Even(arr, even, n);
+	Odd(arr, odd, n);
 	cout << "Массив arr - ";
 	Print(arr, n);
-	
+
 	delete[] even;
 	delete[] odd;
 }
 
-void Arrays_chet_nechet(int arr[], int even[], int odd[], const int n, int cntChetnoe, int cntNeChetnoe)
+void FillRand(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100 + 1;
 	}
-	cout << "Массив even - ";
+}
+void Chetnoe_nechetnoe(int arr[], const int n, int cntChetnoe, int cntNeChetnoe)
+{
 	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] % 2 == 0)
 		{
 			cntChetnoe++;
+		}
+		if (arr[i] % 2 != 0)
+		{
+			cntNeChetnoe++;
+		}
+	}
+	cout << "количество чётных чисел в массиве - " << cntChetnoe << endl;
+	cout << "количество нечётных чисел в массиве - " << cntNeChetnoe << endl;
+}
+void Even(int arr[], int even[], const int n)
+{
+	cout << "Массив even - ";
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
 			even[i] = arr[i];
 			cout << even[i] << " ";
 		}
 	}
+	
+}
+void Odd(int arr[], int odd[], const int n)
+{
 	cout << endl;
 	cout << "Массив odd - ";
 	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] % 2 != 0)
 		{
-			cntNeChetnoe++;
 			odd[i] = arr[i];
 			cout << odd[i] << " ";
 		}
 	}
 	cout << endl;
-	cout << "количество чётных чисел в массиве - " << cntChetnoe << endl;
-	cout << "количество нечётных чисел в массиве - " << cntNeChetnoe << endl;
+	
 }
 void Print(int arr[], const int n)
 {
@@ -63,3 +89,4 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+
