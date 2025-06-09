7f78b4c1e8255a2472fd42a604a2c53ff3ca2717
Danila <Danila@DESKTOP-44LP0PV>
2025-06-10 01:20:14 +0300

    KAKOY_JE_YA_TUPOI!!!!!!!!! SPASITEEEE

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
-int* Push_back(int arr[], int& n,  int value);
+int* Push_back(int arr[], int& n, const int value);
 int* Push_front(int arr[], int& n, const int value);
 
 int* pop_back(int arr[], int& n);
 int* pop_front(int arr[], int& n);
 
+int* Insert(int arr[], int& n, const int value);
+
+void Print_Insert(int arr[], int n);
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -18,7 +21,7 @@ void main()
 	FillRand(arr, n);
 	Print(arr, n);
 	int value;
-	cout << "Введите добавляемое значение: "; cin >> value;
+	/*cout << "Введите добавляемое значение: "; cin >> value;
 	arr = Push_back(arr, n, value);
 	Print(arr, n);
 	
@@ -29,8 +32,12 @@ void main()
 	arr = pop_back(arr, n);
 	Print(arr, n);
 	arr = pop_front(arr, n);
-	Print(arr, n);
+	Print(arr, n);*/
 
+	cout << "Введите добавляемое значение: "; cin >> value;
+
+	arr = Insert(arr, n, value);
+	Print(arr, n);
 	delete[] arr;
 }
 
@@ -51,7 +58,7 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
-int* Push_back(int arr[], int& n,  int value)
+int* Push_back(int arr[], int& n, const int value)
 {
 	//1) Создаём буферный массив нужного размера:
 	int* buffer = new int[n + 1];
@@ -101,4 +108,33 @@ int* pop_front(int arr[], int& n)
 	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
 	delete[] arr;
 	return buffer;
+}
+int* Insert(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	/*arr = buffer;*/
+	int position;
+	cout << "Введите индекс числа: "; cin >> position;
+
+	for (int i = n; i > position; i--)
+	{
+		buffer[i] = buffer[i - 1];
+	}
+	buffer[position] = value;
+
+	n++;
+	return buffer;
+}
+void Print_Insert(int arr[], int n)
+{
+	for (int i = 0; i < n + 1; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
