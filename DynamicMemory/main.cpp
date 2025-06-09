ca8a97338633f7e9cef5b53c8721d6a3b97f00f5
Danila <Danila@DESKTOP-44LP0PV>
2025-06-09 21:22:33 +0300

    Pop_front commit

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+int* Push_back(int arr[], int& n,  int value);
+int* Push_front(int arr[], int& n, const int value);
 
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
 
 void main()
 {
@@ -11,28 +15,22 @@ void main()
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n]; //объявление динамического массива
-
 	FillRand(arr, n);
 	Print(arr, n);
-
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
-
-	//1) Создаём буферный массив нужного размера:
-
-	int* buffer = new int[n + 1];
-
-	//2) Копируем все элементы из исходного массива в буфферный:
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-
-	//3)
-
-	arr[n] = value;
-	n++;
+	arr = Push_back(arr, n, value);
 	Print(arr, n);
+	
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = Push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
 	delete[] arr;
 }
 
@@ -45,11 +43,62 @@ void FillRand(int arr[], const int n)
 }
 void Print(int arr[], const int n)
 {
-	cout << arr << endl;
-	cout << *arr << endl;
+	/*cout << arr << endl;
+	cout << *arr << endl;*/
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t"; // через оператор индексирования
 	}
 	cout << endl;
+}
+int* Push_back(int arr[], int& n,  int value)
+{
+	//1) Создаём буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2) Копируем все элементы из исходного массива в буфферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3) Удаляем исходный массив
+	delete[] arr;
+	//4) Подменяем адрес исходного массива, адресом нового
+	arr = buffer;
+	buffer = nullptr; // nullptr - это указатель на 0;
+	//5) Только после всего вышенаписанного в массиве 'arr' появляется элемент, в который можно сохранить добавляемое значение:
+	arr[n] = value;
+	//6) После добавления элемента в массив, количество его элементов увеличивается на 1
+	n++;
+	return arr;
+}
+int* Push_front(int arr[], int& n, const int value)
+{
+	//1) Создаём буферный массив нужного размера:
+	int* buffer = new int[n + 1];
+	//2) Копируем все элементы из исходного массива в буфферный:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i+1] = arr[i];
+	}
+	//3) Удаляем исходный массив
+	delete[] arr;
+
+	buffer[0] = value;
+	//5)
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
 }
